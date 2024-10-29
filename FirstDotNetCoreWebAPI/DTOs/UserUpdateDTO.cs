25161132ea6a48f51f4fc18a02d398f2d83d5748
Pr0gh4nt3r <github.shortly055@passmail.com>
2024-10-29 02:40:08 +0100

    initial commit

---
+++ FirstDotNetCoreWebAPI/DTOs/UserUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstDotNetCoreWebAPI.DTOs
+{
+    public class UserUpdateDTO
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; } = null;
+        public bool? IsEmailConfirmed { get; set; }
+    }
+}
