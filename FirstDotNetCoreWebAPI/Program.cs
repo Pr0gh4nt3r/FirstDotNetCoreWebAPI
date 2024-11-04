f9a71331f8839da957c5fe4677fff40641ba700c
Pr0gh4nt3r <github.shortly055@passmail.com>
2024-11-05 00:02:34 +0100

    feat(api): add authorization
    - change Helpers to Services

--- FirstDotNetCoreWebAPI/Program.cs
+++ FirstDotNetCoreWebAPI/Program.cs
@@ -1,5 +1,8 @@
 using FirstDotNetCoreWebAPI.Data;
+using FirstDotNetCoreWebAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registriere den UserService im DI-Container
+builder.Services.AddScoped<UserService>(); // AddScoped erstellt eine neue Instanz pro Anfrage
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,                                  // Überprüfe den Aussteller des Tokens
+            ValidateAudience = true,                                // Überprüfe den Zielpublikum des Tokens
+            ValidateLifetime = true,                                // Überprüfe, ob das Token abgelaufen ist
+            ValidateIssuerSigningKey = true,                        // Überprüfe den Signierschlüssel
+            ValidIssuer = builder.Configuration["JWT:Issuer"],      // Hier wird der Issuer aus den Einstellungen gelesen
+            ValidAudience = builder.Configuration["JWT:Audience"],  // Hier wird die Audience aus den Einstellungen gelesen
+            IssuerSigningKey = new SymmetricSecurityKey(Convert.FromHexString(builder.Configuration["JWT:AccessTokenSecret"]))
+        };
+    });
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
@@ -26,6 +48,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
